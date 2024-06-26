f19c097106521d9b842d95e125cc17f9dab74972
Jay Teramoto <jayteramoto@gmail.com>
2024-06-26 10:06:56 +0900

    Change M0/M1 pin assign

--- Serial_bridge_HardwareSerilal_ESP32_for_E220.ino
+++ Serial_bridge_HardwareSerilal_ESP32_for_E220.ino
@@ -8,14 +8,31 @@ PLatform is ESP-WROOM-32D
 For binary data transfer, use RS232 TEST tools
 http://nonsoft.la.coocan.jp/Download/Rs232cTool/index.html
 
+設定確認
+送-> <c1><00><08>
+->受 <c0><00><08><00><00><70><01><00><C5><00><00>
 
+※9バイトを読み込めばバージョン番号取得
+
+設定コマンド
+送-> <c0><00><08><<00><00><70><01><00><C5><00><00>
+->受 <c1><00><08><00><00><70><01><00><C5><00><00>
+
+<現在の設定>
+接続 COM10
+送-> <c1><00><09>
+->受 <c1><00><09><00><00><70><01><00><c5><00><00><10>
+
+Rev 0.2 2024.06.25  Change M0/M1 pins
+Rev 0.3 2024.06.26  Cleanup codes, add config commands
 Jay Teramoto 2024.6.21 (Complete)
 */
 
-#define M0pin   27      // GPIO27
-#define M1pin   14      // GPIO14
-#define Tx      17      // GPIO17
-#define Rx      16      // GPIO16
+#define M0pin   26      // New assign
+#define M1pin   27      // New assign
+#define AUXpin  14      // New assign (not use)
+#define Tx      17      // GPIO17 U2TDX (not use)
+#define Rx      16      // GPIO16 U2RDX (not use)
 
 void setup() {
 
@@ -27,6 +44,7 @@ void setup() {
   
   Serial.begin(9600);
   Serial2.begin(9600);
+  delay(1000);
   Serial.println("E220 Config read/write via HardwareSerial2 on ESP32 ");
 
 }
